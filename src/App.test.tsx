d9d08cdccd148e6269e301e26735dbdfc99c3c40
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-11-01 18:22:04 +0500

    Wrote test cases for app root component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,21 @@
-import Enzyme from "enzyme";
-import ReactSixteenAdaptor from "enzyme-adapter-react-16"
+import React from "react"
+import {shallow, mount} from "enzyme"
+// Component.
+import GridComponent from "./components/Grid/GridComponent"
+import App from "./App"
 
-Enzyme.configure({adapter: new ReactSixteenAdaptor()})
+describe("App root component", ()=>{
+    let container: any;
+
+    beforeEach(()=>(
+        container = shallow(<App/>)
+    ))
+
+    it("at least one div to be found", ()=>{
+        expect(container.find("div").length).toEqual(1)
+    })
+
+    it("check if Grid component has been rendered", ()=>{
+        expect(container.containsMatchingElement(<GridComponent/>)).toEqual(true)
+    })
+})
